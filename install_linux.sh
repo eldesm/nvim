fd84a6193434c76759c1673987284e64d9f26ab5
Eli <eli.desmet@braincreators.com>
2021-07-29 17:02:38 +0200

    Improvement in installer

--- install_linux.sh
+++ install_linux.sh
@@ -1,7 +1,8 @@
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O nvim.appimage
 chmod u+x nvim.appimage
 sudo rm /usr/bin/nvim
-sudo rm /.config/nvim/init.vim
+mkdir -p ~/.config/nvim
+sudo rm ~/.config/nvim/init.vim
 sudo ln -s $PWD/nvim.appimage /usr/bin/nvim
 sudo ln -s $PWD/init.vim ~/.config/nvim/init.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
