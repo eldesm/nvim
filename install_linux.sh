b80ddca8be58f15f4d094d07307ae6704e5439c7
Eli <eli.desmet@braincreators.com>
2021-07-29 16:48:38 +0200

    Improvement to installer

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,7 @@
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O nvim.appimage
 chmod u+x nvim.appimage
 sudo rm /usr/bin/nvim
+sudo rm /.config/nvim/init.vim
 sudo ln -s $PWD/nvim.appimage /usr/bin/nvim
 sudo ln -s $PWD/init.vim ~/.config/nvim/init.vim
+nvim --headless +PlugInstall +qall
